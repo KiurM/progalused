972e3ea1c752eaec005c582216f88b26785c8364
Kiur Mölder <kiur.molder.001@voco.ee>
2022-10-27 14:43:50 +0300

    Ülesanded tehtud

---
+++ Funktsioon.py
@@ -0,0 +1,36 @@
+"""Ülesanne: Funktsioon"""
+
+# Ülesanne 1
+
+def func():
+    print("I´m inside the function")
+
+func()
+
+# Ülesanne 2
+
+def my_name_is(name):
+    print(f"My name is {name}")
+
+my_name_is("Emili")
+
+# Ülesanne 3
+
+def sum_six(num):
+    num = print(int(num) + int(6))
+    return num
+sum_six(int(6))
+
+# Ülesanne 4
+
+def  sum_numbers(a, b):
+    print(int(a) + int(b))
+
+sum_numbers(15, 5)
+
+# Ülesanne 5
+
+def usd_to_eur(usd):
+    print(int(usd) - (int(usd) / 100 * 20))
+
+usd_to_eur(8)
