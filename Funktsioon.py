6258301219e41bd92bcfad62666d526d5be8e700
Kiur Mölder <kiur.molder.001@voco.ee>
2022-11-11 15:25:07 +0200

    D:

--- Funktsioon.py
+++ Funktsioon.py
@@ -1,36 +1,37 @@
-"""Ülesanne: Funktsioon"""
+"""Ülesanne: Funktsioon."""
 
-# Ülesanne 1
 
 def func():
     print("I´m inside the function")
 
+
 func()
 
-# Ülesanne 2
 
 def my_name_is(name):
     print(f"My name is {name}")
 
+
 my_name_is("Emili")
 
-# Ülesanne 3
 
-def sum_six(num):
-    num = print(int(num) + int(6))
+def sum_six(num: int):
+    num = num + 6
     return num
-sum_six(int(6))
 
-# Ülesanne 4
 
-def  sum_numbers(a, b):
+sum_six(6)
+
+
+def sum_numbers(a, b):
     return (int(a) + int(b))
+
 
 print(sum_numbers(15, 5))
 
-# Ülesanne 5
 
-def usd_to_eur(usd):
-    return(int(usd) - (int(usd) / 100 * 20))
+def usd_to_eur(usd: int):
+    return((usd) - ((usd) / 100 * 20)) 
 
-print(usd_to_eur(10))
+
+print(usd_to_eur(100))
